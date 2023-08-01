440927b2e2eca50dee2b1afc87fdbfcf46b109a7
Ethan <eman5797@gmail.com>
2023-08-01 10:48:57 -0400

    fixed button position

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -11,25 +11,55 @@ const AddButton = () => {
             position={'fixed'}
             height={'100vh'}
             width={'100vw'}
+            display={'flex'}
+            flexDirection={'column'}
+            justifyContent={'flex-end'}
+            // alignItems={'center'}
+            bottom={3}
+        // pr={20}
         >
-            <IconButton
-                sx={{
-                    backgroundColor: 'rgba(255,255,255,1)',
-                    position: 'absolute',
-                    right: 30,
-                    bottom: 30,
-                    height: '75px',
-                    width: '75px',
-                    zIndex: 2
-                }}
-                variant="contained"
-                disableRipple
-                onClick={() => setOpenModal(true)}
+            <Box
+                height={'125px'}
+                width={'115px'}
+                backgroundColor={'#070417'}
+                // right={130}
+                position={'absolute'}
+                bottom={-10}
+                borderRadius={'0 60px 0 0'}
+
+            />
+            <Box
+                width={'115px'}
+                height={'115px'}
+                backgroundColor={'rgb(63,60,77)'}
+                // bottom={10}
+                position={'absolute'}
+                borderRadius={'100px'}
+                display={'flex'}
+                justifyContent={'center'}
+                alignItems={'center'}
+            // right={130}
             >
-                <AddIcon
-                    fontSize="large"
-                />
-            </IconButton>
+                <IconButton
+                    sx={{
+                        backgroundColor: 'rgba(255,255,255,1)',
+                        // position: 'absolute',
+                        // right: 80,
+                        // bottom: 30,
+                        height: '75px',
+                        width: '75px',
+                        zIndex: 2
+                    }}
+                    variant="contained"
+                    disableRipple
+                    onClick={() => setOpenModal(true)}
+                >
+                    <AddIcon
+                        fontSize="large"
+                        color="primary"
+                    />
+                </IconButton>
+            </Box>
             <Modal
                 open={openModal}
                 onClose={() => setOpenModal(false)}
